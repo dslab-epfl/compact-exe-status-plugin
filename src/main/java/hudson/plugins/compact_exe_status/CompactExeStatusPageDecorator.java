b15f595c8386948cad2300d0c5898b6aa7b79b5f
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2013-01-24 13:59:02 +0100

    Initial commit

---
+++ src/main/java/hudson/plugins/compact_exe_status/CompactExeStatusPageDecorator.java
@@ -0,0 +1,8 @@
+package hudson.plugins.compact_exe_status;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+@Extension
+public class CompactExeStatusPageDecorator extends PageDecorator {
+}
